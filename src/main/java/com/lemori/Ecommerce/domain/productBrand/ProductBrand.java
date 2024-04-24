bddb4a2823c602f11240a055393a3e586120a6f8
Lemos <gabriel_lemos06@hotmail.com>
2024-04-23 21:42:47 -0300

    [core]: adding Product brand entity.

---
+++ src/main/java/com/lemori/Ecommerce/domain/productBrand/ProductBrand.java
@@ -0,0 +1,27 @@
+package com.lemori.Ecommerce.domain.productBrand;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "products_brands")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductBrand {
+
+    @Id
+    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+}
